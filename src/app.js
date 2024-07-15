6c8e0a5946253f1824c990cf9f86316e48d58e52
Elías JR <167987056+eliasjr89@users.noreply.github.com>
2024-07-15 12:25:24 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "My", "Your"];
+  let adjs = ["great", "big", "blue"];
+  let nouns = ["jogger", "racoon", "pc"];
+
+  function posiblesCombinaciones(pronouns, adjs, nouns) {
+    let combinaciones = [];
+
+    for (let p = 0; p < pronouns.length; p++) {
+      for (let a = 0; a < adjs.length; a++) {
+        for (let n = 0; n < nouns.length; n++) {
+          combinaciones.push(`${pronouns[p]}${adjs[a]}${nouns[n]}`);
+        }
+      }
+    }
+
+    return combinaciones;
+  }
+
+  const button = document.getElementById("generate-button");
+  const resultList = document.getElementById("result-list");
+
+  button.addEventListener("click", () => {
+    resultList.innerHTML = "";
+
+    let combinaciones = posiblesCombinaciones(pronouns, adjs, nouns);
+
+    combinaciones.forEach(combinacion => {
+      let listItem = document.createElement("li");
+      listItem.textContent = combinacion;
+      resultList.appendChild(listItem);
+    });
+  });
 };
